66af47e45297cbf6ebf2d67fde1031da7acde63e
Erwin Flores <ramiromarca6@gmail.com>
2021-04-08 00:10:53 -0400

    sigma

--- src/App.vue
+++ src/App.vue
@@ -1,248 +1,258 @@
 <template>
-  <IonApp>
-    <IonSplitPane content-id="main-content">
-      <ion-menu content-id="main-content" type="overlay">
-        <ion-content>
-          <ion-list id="inbox-list">
-            <ion-list-header>Inbox</ion-list-header>
-            <ion-note>hi@ionicframework.com</ion-note>
-  
-            <ion-menu-toggle auto-hide="false" v-for="(p, i) in appPages" :key="i">
-              <ion-item @click="selectedIndex = i" router-direction="root" :router-link="p.url" lines="none" detail="false" class="hydrated" :class="{ selected: selectedIndex === i }">
-                <ion-icon slot="start" :ios="p.iosIcon" :md="p.mdIcon"></ion-icon>
-                <ion-label>{{ p.title }}</ion-label>
-              </ion-item>
-            </ion-menu-toggle>
-          </ion-list>
-  
-          <ion-list id="labels-list">
-            <ion-list-header>Labels</ion-list-header>
-  
-            <ion-item v-for="(label, index) in labels" lines="none" :key="index">
-              <ion-icon slot="start" :ios="bookmarkOutline" :md="bookmarkSharp"></ion-icon>
-              <ion-label>{{ label }}</ion-label>
-            </ion-item>
-          </ion-list>
-        </ion-content>
-      </ion-menu>
-      <ion-router-outlet id="main-content"></ion-router-outlet>
-    </IonSplitPane>
-  </IonApp>
+	<div :class="containerClass" @click="onWrapperClick">
+		<AppTopBar @menu-toggle="onMenuToggle" />
+
+        <transition name="layout-sidebar">
+            <div :class="sidebarClass" @click="onSidebarClick" v-show="isSidebarVisible()">
+                <div class="layout-logo">
+                    <router-link to="/">
+                        <img alt="Logo" :src="logo" />
+                    </router-link>
+                </div>
+
+                <AppProfile />
+                <AppMenu :model="menu" @menuitem-click="onMenuItemClick" />
+            </div>
+        </transition>
+
+		<div class="layout-main">
+			<router-view />
+		</div>
+
+		<AppConfig :layoutMode="layoutMode" :layoutColorMode="layoutColorMode" @layout-change="onLayoutChange" @layout-color-change="onLayoutColorChange"/>
+
+		<AppFooter />
+	</div>
 </template>
 
-<script lang="ts">
-import { IonApp, IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle, IonNote, IonRouterOutlet, IonSplitPane } from '@ionic/vue';
-import { defineComponent, ref } from 'vue';
-import { useRoute } from 'vue-router';
-import { archiveOutline, archiveSharp, bookmarkOutline, bookmarkSharp, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+<script>
+import AppTopBar from './AppTopbar.vue';
+import AppProfile from './AppProfile.vue';
+import AppMenu from './AppMenu.vue';
+import AppConfig from './AppConfig.vue';
+import AppFooter from './AppFooter.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    IonApp, 
-    IonContent, 
-    IonIcon, 
-    IonItem, 
-    IonLabel, 
-    IonList, 
-    IonListHeader, 
-    IonMenu, 
-    IonMenuToggle, 
-    IonNote, 
-    IonRouterOutlet, 
-    IonSplitPane,
-  },
-  setup() {
-    const selectedIndex = ref(0);
-    const appPages = [
-      {
-        title: 'Inbox',
-        url: '/folder/Inbox',
-        iosIcon: mailOutline,
-        mdIcon: mailSharp
-      },
-      {
-        title: 'Outbox',
-        url: '/folder/Outbox',
-        iosIcon: paperPlaneOutline,
-        mdIcon: paperPlaneSharp
-      },
-      {
-        title: 'Favorites',
-        url: '/folder/Favorites',
-        iosIcon: heartOutline,
-        mdIcon: heartSharp
-      },
-      {
-        title: 'Archived',
-        url: '/folder/Archived',
-        iosIcon: archiveOutline,
-        mdIcon: archiveSharp
-      },
-      {
-        title: 'Trash',
-        url: '/folder/Trash',
-        iosIcon: trashOutline,
-        mdIcon: trashSharp
-      },
-      {
-        title: 'Spam',
-        url: '/folder/Spam',
-        iosIcon: warningOutline,
-        mdIcon: warningSharp
-      }
-    ];
-    const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-    
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      selectedIndex.value = appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+export default {
+    data() {
+        return {
+            layoutMode: 'static',
+            layoutColorMode: 'dark',
+            staticMenuInactive: false,
+            overlayMenuActive: false,
+            mobileMenuActive: false,
+            menu : [
+                {label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/'},
+				{
+					label: 'UI Kit', icon: 'pi pi-fw pi-sitemap',
+					items: [
+						{label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/formlayout'},
+						{label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/input'},
+                        {label: "Float Label", icon: "pi pi-fw pi-bookmark", to: "/floatlabel"},
+                        {label: "Invalid State", icon: "pi pi-fw pi-exclamation-circle", to: "invalidstate"},
+						{label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/button'},
+						{label: 'Table', icon: 'pi pi-fw pi-table', to: '/table'},
+						{label: 'List', icon: 'pi pi-fw pi-list', to: '/list'},
+						{label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/tree'},
+						{label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/panel'},
+						{label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/overlay'},
+						{label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/menu'},
+						{label: 'Message', icon: 'pi pi-fw pi-comment', to: '/messages'},
+						{label: 'File', icon: 'pi pi-fw pi-file', to: '/file'},
+						{label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/chart'},
+						{label: 'Misc', icon: 'pi pi-fw pi-circle-off', to: '/misc'},
+					]
+				},
+				{
+					label: "Utilities", icon:'pi pi-fw pi-globe',
+					items: [
+						{label: 'Display', icon:'pi pi-fw pi-desktop', to:'/display'},
+						{label: 'Elevation', icon:'pi pi-fw pi-external-link', to:'/elevation'},
+						{label: 'Flexbox', icon:'pi pi-fw pi-directions', to:'/flexbox'},
+						{label: 'Icons', icon:'pi pi-fw pi-search', to:'/icons'},
+						{label: 'Grid System', icon:'pi pi-fw pi-th-large', to:'/grid'},
+						{label: 'Spacing', icon:'pi pi-fw pi-arrow-right', to:'/spacing'},
+						{label: 'Typography', icon:'pi pi-fw pi-align-center', to:'/typography'},
+						{label: 'Text', icon:'pi pi-fw pi-pencil', to:'/text'},
+					]
+				},
+				{
+					label: 'Pages', icon: 'pi pi-fw pi-clone',
+					items: [
+						{label: 'Crud', icon: 'pi pi-fw pi-user-edit', to: '/crud'},
+						{label: 'Calendar', icon: 'pi pi-fw pi-calendar-plus', to: '/calendar'},
+						{label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/timeline'},
+						{label: 'Empty Page', icon: 'pi pi-fw pi-circle-off', to: '/empty'}
+					]
+				},
+                {
+                    label: 'Menu Hierarchy', icon: 'pi pi-fw pi-search',
+                    items: [
+                        {
+                            label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                {
+                                    label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
+                                    items: [
+                                        {label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark'},
+                                        {label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark'},
+                                        {label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark'},
+                                    ]
+                                },
+                                {
+                                    label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
+                                    items: [
+                                        {label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark'},
+                                        {label: 'Submenu 1.2.2', icon: 'pi pi-fw pi-bookmark'}
+                                    ]
+                                },
+                            ]
+                        },
+                        {
+                            label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
+                            items: [
+                                {
+                                    label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
+                                    items: [
+                                        {label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark'},
+                                        {label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark'},
+                                        {label: 'Submenu 2.1.3', icon: 'pi pi-fw pi-bookmark'},
+                                    ]
+                                },
+                                {
+                                    label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
+                                    items: [
+                                        {label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark'},
+                                        {label: 'Submenu 2.2.2', icon: 'pi pi-fw pi-bookmark'}
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {label: 'Documentation', icon: 'pi pi-fw pi-question', command: () => {window.location = "#/documentation"}},
+                {label: 'View Source', icon: 'pi pi-fw pi-search', command: () => {window.location = "https://github.com/primefaces/sigma-vue"}}
+            ]
+        }
+    },
+    watch: {
+        $route() {
+            this.menuActive = false;
+            this.$toast.removeAllGroups();
+        }
+    },
+    methods: {
+        onWrapperClick() {
+            if (!this.menuClick) {
+                this.overlayMenuActive = false;
+                this.mobileMenuActive = false;
+            }
+
+            this.menuClick = false;
+        },
+        onMenuToggle() {
+            this.menuClick = true;
+
+            if (this.isDesktop()) {
+                if (this.layoutMode === 'overlay') {
+					if(this.mobileMenuActive === true) {
+						this.overlayMenuActive = true;
+					}
+
+                    this.overlayMenuActive = !this.overlayMenuActive;
+					this.mobileMenuActive = false;
+                }
+                else if (this.layoutMode === 'static') {
+                    this.staticMenuInactive = !this.staticMenuInactive;
+                }
+            }
+            else {
+                this.mobileMenuActive = !this.mobileMenuActive;
+            }
+
+            event.preventDefault();
+        },
+        onSidebarClick() {
+            this.menuClick = true;
+        },
+        onMenuItemClick(event) {
+            if (event.item && !event.item.items) {
+                this.overlayMenuActive = false;
+                this.mobileMenuActive = false;
+            }
+        },
+		onLayoutChange(layoutMode) {
+			this.layoutMode = layoutMode;
+		},
+		onLayoutColorChange(layoutColorMode) {
+			this.layoutColorMode = layoutColorMode;
+		},
+        addClass(element, className) {
+            if (element.classList)
+                element.classList.add(className);
+            else
+                element.className += ' ' + className;
+        },
+        removeClass(element, className) {
+            if (element.classList)
+                element.classList.remove(className);
+            else
+                element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        },
+        isDesktop() {
+            return window.innerWidth > 1024;
+        },
+        isSidebarVisible() {
+            if (this.isDesktop()) {
+                if (this.layoutMode === 'static')
+                    return !this.staticMenuInactive;
+                else if (this.layoutMode === 'overlay')
+                    return this.overlayMenuActive;
+                else
+                    return true;
+            }
+            else {
+                return true;
+            }
+        },
+    },
+    computed: {
+        containerClass() {
+            return ['layout-wrapper', {
+                'layout-overlay': this.layoutMode === 'overlay',
+                'layout-static': this.layoutMode === 'static',
+                'layout-static-sidebar-inactive': this.staticMenuInactive && this.layoutMode === 'static',
+                'layout-overlay-sidebar-active': this.overlayMenuActive && this.layoutMode === 'overlay',
+                'layout-mobile-sidebar-active': this.mobileMenuActive,
+				'p-input-filled': this.$appState.inputStyle === 'filled',
+				'p-ripple-disabled': this.$primevue.config.ripple === false
+            }];
+        },
+        sidebarClass() {
+            return ['layout-sidebar', {
+                'layout-sidebar-dark': this.layoutColorMode === 'dark',
+                'layout-sidebar-light': this.layoutColorMode === 'light'
+            }];
+        },
+        logo() {
+            return (this.layoutColorMode === 'dark') ? "assets/layout/images/logo-white.svg" : "assets/layout/images/logo.svg";
+        }
+    },
+    beforeUpdate() {
+        if (this.mobileMenuActive)
+            this.addClass(document.body, 'body-overflow-hidden');
+        else
+            this.removeClass(document.body, 'body-overflow-hidden');
+    },
+    components: {
+        'AppTopBar': AppTopBar,
+        'AppProfile': AppProfile,
+        'AppMenu': AppMenu,
+        'AppConfig': AppConfig,
+        'AppFooter': AppFooter,
     }
-    
-    const route = useRoute();
-    
-    return { 
-      selectedIndex,
-      appPages, 
-      labels,
-      archiveOutline, 
-      archiveSharp, 
-      bookmarkOutline, 
-      bookmarkSharp, 
-      heartOutline, 
-      heartSharp, 
-      mailOutline, 
-      mailSharp, 
-      paperPlaneOutline, 
-      paperPlaneSharp, 
-      trashOutline, 
-      trashSharp, 
-      warningOutline, 
-      warningSharp,
-      isSelected: (url: string) => url === route.path ? 'selected' : ''
-    }
-  }
-});
+}
 </script>
 
-<style scoped>
-ion-menu ion-content {
-  --background: var(--ion-item-background, var(--ion-background-color, #fff));
-}
-
-ion-menu.md ion-content {
-  --padding-start: 8px;
-  --padding-end: 8px;
-  --padding-top: 20px;
-  --padding-bottom: 20px;
-}
-
-ion-menu.md ion-list {
-  padding: 20px 0;
-}
-
-ion-menu.md ion-note {
-  margin-bottom: 30px;
-}
-
-ion-menu.md ion-list-header,
-ion-menu.md ion-note {
-  padding-left: 10px;
-}
-
-ion-menu.md ion-list#inbox-list {
-  border-bottom: 1px solid var(--ion-color-step-150, #d7d8da);
-}
-
-ion-menu.md ion-list#inbox-list ion-list-header {
-  font-size: 22px;
-  font-weight: 600;
-
-  min-height: 20px;
-}
-
-ion-menu.md ion-list#labels-list ion-list-header {
-  font-size: 16px;
-
-  margin-bottom: 18px;
-
-  color: #757575;
-
-  min-height: 26px;
-}
-
-ion-menu.md ion-item {
-  --padding-start: 10px;
-  --padding-end: 10px;
-  border-radius: 4px;
-}
-
-ion-menu.md ion-item.selected {
-  --background: rgba(var(--ion-color-primary-rgb), 0.14);
-}
-
-ion-menu.md ion-item.selected ion-icon {
-  color: var(--ion-color-primary);
-}
-
-ion-menu.md ion-item ion-icon {
-  color: #616e7e;
-}
-
-ion-menu.md ion-item ion-label {
-  font-weight: 500;
-}
-
-ion-menu.ios ion-content {
-  --padding-bottom: 20px;
-}
-
-ion-menu.ios ion-list {
-  padding: 20px 0 0 0;
-}
-
-ion-menu.ios ion-note {
-  line-height: 24px;
-  margin-bottom: 20px;
-}
-
-ion-menu.ios ion-item {
-  --padding-start: 16px;
-  --padding-end: 16px;
-  --min-height: 50px;
-}
-
-ion-menu.ios ion-item.selected ion-icon {
-  color: var(--ion-color-primary);
-}
-
-ion-menu.ios ion-item ion-icon {
-  font-size: 24px;
-  color: #73849a;
-}
-
-ion-menu.ios ion-list#labels-list ion-list-header {
-  margin-bottom: 8px;
-}
-
-ion-menu.ios ion-list-header,
-ion-menu.ios ion-note {
-  padding-left: 16px;
-  padding-right: 16px;
-}
-
-ion-menu.ios ion-note {
-  margin-bottom: 8px;
-}
-
-ion-note {
-  display: inline-block;
-  font-size: 16px;
-
-  color: var(--ion-color-medium-shade);
-}
-
-ion-item.selected {
-  --color: var(--ion-color-primary);
-}
+<style lang="scss">
+@import './App.scss';
 </style>
